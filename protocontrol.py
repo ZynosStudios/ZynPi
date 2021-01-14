d2de7afd10136ba43db904a7c6ddf5ef100e0253
RadenTheFolf <radenthefolf@gmail.com>
2021-01-14 01:47:26 -0500

    added basic project structure

--- protocontrol.py
+++ protocontrol.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
